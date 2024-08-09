34609756fa7f8e0087d40bec3f83a7e891b2c3da
pushkal2612 <89384010+pushkal2612@users.noreply.github.com>
2024-08-09 16:57:48 +0530

    first commit

--- 01_variables/test.js
+++ 01_variables/test.js
@@ -1,1 +1,11 @@
-console.log("hellow");
+const accountnumber=12345
+console.log(accountnumber);
+/* accountnumber=23456 */
+var accountname="string"
+let accountemail="pushkal@gmail.com"
+accountpassword=123
+
+console.table([accountnumber,accountname,accountemail,accountpassword]);
+/* prefered not to use var
+    because of issue in block scope and function scope
+*/
