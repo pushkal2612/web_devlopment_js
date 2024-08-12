e95efdca908f4a84325aad8f2237387ca437bc23
pushkal2612 <89384010+pushkal2612@users.noreply.github.com>
2024-08-12 16:54:33 +0530

    first commit

---
+++ 02_datatypes/index.js
@@ -0,0 +1,7 @@
+let name="pushkal";
+let age=29
+let isLogin=false
+let sub=null
+let fee=undefined
+console.log(typeof(sub));
+console.log(typeof(fee));
---
+++ 03_datatype_conversion/index.js
@@ -0,0 +1,55 @@
+/* let score="33abc";
+let valueInNumber=Number(score)
+console.log(valueInNumber); */
+/* let score=0;
+console.log(score);
+console.log(typeof(score)); */
+/* let isLogin="";
+let abx=Boolean(isLogin);
+console.log(typeof(abx));
+console.log(abx); */
+/* let somenumber=33;
+let stringNumber=String(somenumber);
+console.log(typeof(stringNumber));
+console.log(stringNumber); */
+
+/****Opertions******/
+/* let value=3;
+let negative=-value;
+console.log(negative); */
+
+/* console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**100); */
+
+/* let str1="hellow"
+let str2=" pushkal"
+let str3=str1+str2
+console.log(str3); */
+
+/* console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 +2 + "2");
+console.log("1" + (2 + 2)); */
+
+/* num1=num2=num3=2*2;
+console.log(num1); */
+
+let gamecounter=100;
+
+console.log(++gamecounter);
+
+
+
+
+
+
+
+
+
+
+
