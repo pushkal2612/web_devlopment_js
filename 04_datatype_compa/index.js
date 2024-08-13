0d920e89538a49b321658a465962e7b9fcb69da0
pushkal2612 <89384010+pushkal2612@users.noreply.github.com>
2024-08-13 17:08:14 +0530

    first commit

---
+++ 04_datatype_compa/index.js
@@ -0,0 +1,26 @@
+/* console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2!=2);
+console.log(2==2); */
+
+/* console.log("2" > 1);
+console.log("02" > 1); */
+
+/* console.log(null>=0);
+console.log(null<0);
+console.log(null == 0); */
+
+/* conversion null value convert in number as o */
+/* equation like equality in that case the null value is not 0 */
+/* comparision < , > ,<= ,>= null value convert in 0 */
+/* 
+console.log(undefined>=0);
+console.log(undefined<0);
+console.log(undefined == 0); */
+
+/* strict check === */
+/* let score= 100;
+let name="100"
+console.log(score===name); */
---
+++ 05_datatype_summary/index.js
@@ -0,0 +1,25 @@
+/* let score=100;
+let scorevalue=100.3
+let isLoggedIn= false
+let outsidetemp=null;
+let useremail;
+let id=Symbol('123');
+let anotherid=Symbol('123'); */
+/* 
+let bignumber=42389547285738929n
+console.log(typeof bignumber); */
+
+
+let newarray=["ironman","hulk","thor"]
+let myobj={
+    name:"pushkal",
+    age:29
+}
+/* function(){} */
+
+let myfunction=function(){
+    console.log("hellow world");
+    
+}
+
+console.log(typeof newarray);
